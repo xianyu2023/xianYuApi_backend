a848850183ce0e2a4caf7cbb522e31e54de8d7c9
xianyu <1802698990@qq.com>
2023-09-28 23:32:43 +0800

    支持聚合搜索、数据抓取、数据同步

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -27,21 +27,40 @@ create table user
         unique (userAccount)
 )
     comment '用户' charset = utf8;
-
-
-
--- 帖子表
-create table if not exists post
+-- 开放接口信息
+-- auto-generated definition
+create table open_api
 (
-    id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
-    content    text                               null comment '内容',
-    tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    thumbNum   int      default 0                 not null comment '点赞数',
-    favourNum  int      default 0                 not null comment '收藏数',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
-    index idx_userId (userId)
-) comment '帖子' collate = utf8mb4_unicode_ci;
+    id             bigint auto_increment comment '主键'
+        primary key,
+    name           varchar(256)        not null comment '接口名称',
+    description    varchar(256)        null comment '接口描述',
+    url            varchar(512)        not null comment '接口地址',
+    method         varchar(256)        null comment '请求类型',
+    requestParams  text                null comment '请求参数',
+    requestHeader  text                null comment '请求头',
+    responseHeader text                null comment '响应头',
+    status         int      default 0                 not null comment '接口状态（0-关闭，1-开启）',
+    userId         bigint                             not null comment '创建人id',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDeleted      tinyint  default 0                 not null comment '是否删除(0-未删, 1-已删)',
+    path           varchar(256)                       null comment 'API的请求路径'
+)
+    comment '开放接口信息' charset = utf8;
+
+-- 用户调用接口关系表：
+create table user_open_api
+(
+    id             bigint auto_increment comment '主键'
+        primary key,
+    userId         bigint                             not null comment '调用者id',
+    openApiId         bigint                             not null comment '接口id',
+    totalNum         int      default 0                 not null comment '总调用次数',
+    leftNum         int      default 0                 not null comment '剩余调用次数',
+    status         int      default 0                 not null comment '调用权限（0-正常，1-禁止）',
+    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDeleted      tinyint  default 0                 not null comment '是否删除(0-未删, 1-已删)'
+)
+    comment '用户调用接口关系表'
